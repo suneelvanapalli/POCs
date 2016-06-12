2d22df78019024340251c3c33a95bdeaea8d9c2b
suneelvanapalli <suneel.vanapalli@gmail.com>
2016-06-12 10:15:58 +1000

    Setting up project for IOC, unit testing , mocking framework

---
+++ SetupIOCUnitTestingMock/SetupIOCUnitTestingMock/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SetupIOCUnitTestingMock.Startup))]
+namespace SetupIOCUnitTestingMock
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
