a3d8a6b4b46cf4f67ba5dd3506de694ffdc39427
suneelvanapalli <suneel.vanapalli@gmail.com>
2016-06-19 10:25:26 +1000

    aynsc await sample

---
+++ AsyncAwaitSample/AsyncAwaitSample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace AsyncAwaitSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main method before async call" + Thread.CurrentThread.ManagedThreadId);
+
+            var accesswebtask =  AccessTheWebAsync();
+
+            Console.WriteLine("Main method after aync call" + Thread.CurrentThread.ManagedThreadId);
+            Console.ReadKey();
+        }
+
+        static async Task<int> AccessTheWebAsync()
+        {
+            var client = new HttpClient();
+            var getStringTask = client.GetStringAsync("http://msdn.microsoft.com");
+            DoIndependentWork();
+
+            string urlContents = await getStringTask;
+
+            return urlContents.Length;
+
+        }
+        static void DoIndependentWork()
+        {
+            Console.WriteLine("Doing independent work" + Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
