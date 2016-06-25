2d2e07a7ed607487c3d4345974328f5e721ee54d
suneelvanapalli <suneel.vanapalli@gmail.com>
2016-06-25 11:17:33 +1000

    Script file for creating root and other certificates from root.

---
+++ CertificateSetup/CreateDevRoot.cmd
@@ -0,0 +1,14 @@
+makecert.exe -r 
+	-n "CN=DevRoot" 
+	-pe 
+	-sv DevRoot.pvk 
+	-a sha1 
+	-len 2048 
+	-b 01/21/2016 
+	-e 01/21/2030 
+	-cy authority DevRoot.cer
+
+pvk2pfx.exe 
+	-pvk DevRoot.pvk 
+	-spc DevRoot.cer 
+	-pfx DevRoot.pfx
