de7e5baf6ee9669faa822bb52048d74c0298aba1
suneelvanapalli <suneel.vanapalli@gmail.com>
2016-07-23 15:30:06 +1000

    Basic setup of Reverse poco generator and creating DBContext

---
+++ EFCodeBasedModellingDemo/EFCodeBasedModellingDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCodeBasedModellingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new NorthWindDBContext())
+            {
+                var data = db.Categories;
+
+                foreach(var c in data)
+                {
+                    Console.WriteLine(c.CategoryName);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
