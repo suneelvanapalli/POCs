fc99ed0440764266d59ca5bc777366387ec52515
suneelvanapalli <suneel.vanapalli@gmail.com>
2016-09-10 19:32:05 +1000

    test change

--- PS Commands/Get Latest and Build Solution.ps1
+++ PS Commands/Get Latest and Build Solution.ps1
@@ -2,6 +2,23 @@
 $portalClientSolutionName = "NumericSequenceCalculator.sln"
 
 Set-Location $portalClientSolutionPath
-#git checkout develop
- 
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe $portalClientSolutionName /t:Build /p:Configuration=Release
+
+getlatest("master")
+
+#C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe $portalClientSolutionName /t:Build /p:Configuration=Release
+
+function getlatest($branchName){
+
+  #  ECHO Discard changes
+  #  git reset --hard
+
+    ECHO GIT CHECKOUT MASTER
+    git checkout $branchName
+
+    git pull
+
+    ECHO 'GIT STATUS'
+    git status    
+
+}
+
