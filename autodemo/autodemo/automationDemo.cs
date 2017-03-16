ba8f6836058e0542c7ec8b60695bcbc524934b99
suneelvanapalli <suneel.vanapalli@gmail.com>
2017-03-16 23:06:29 +1100

    Automation Demo success!

---
+++ autodemo/autodemo/automationDemo.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autodemo
+{
+    [TestFixture()]
+    public class automationDemo
+    {
+        [Test()]
+        public void OpenBrowser()
+        {
+            var driver = new ChromeDriver();
+            
+            driver.Navigate().GoToUrl("https://app.pluralsight.com/id?redirectTo=/");
+
+            var element = driver.FindElementsByName("Username").Single();
+            element.SendKeys("suneelkumarv");
+
+            element = driver.FindElementsByName("Password").Single();
+            element.SendKeys("password1!");
+
+            element.Submit();
+
+            //element = driver.FindElementsByName("prism-search-input").Single();
+           // element.SendKeys("BDD");
+
+            //element.Submit();
+
+            driver.Close();
+
+        }
+    }
+}
