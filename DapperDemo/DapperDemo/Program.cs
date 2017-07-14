ba06416e8211285eedd4d01b36fa331030bcdee7
SUNEELKUMARV\suneelkumarv <suneel.vanapalli@gmail.com>
2017-07-14 11:32:39 +1000

    read query from db using dapper

--- DapperDemo/DapperDemo/Program.cs
+++ DapperDemo/DapperDemo/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Dapper;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
 
 namespace DapperDemo
 {
@@ -6,7 +10,22 @@ namespace DapperDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Dapper!");
+
+            using (IDbConnection db = new SqlConnection("Server=.;Database=Demo;Integrated Security=true"))
+
+            {
+
+                var  list =  db.Query<Vendor>("Select * From Vendor").ToList();
+
+            }
+            Console.ReadKey();
         }
+    }
+
+    class Vendor
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
     }
 }
